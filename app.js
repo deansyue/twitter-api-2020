5b38708d20167533c50b35a325bc750fcc6b2bf9
hmrvc <hmrvc@yahoo.com.tw>
2022-02-25 09:04:17 +0800

    conflict check

--- app.js
+++ app.js
@@ -16,19 +16,10 @@ const port = process.env.PORT || 3000
 // 初始化passport
 app.use(passport.initialize())
 
-<<<<<<< HEAD
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // 設定讓外部傳入的 request 可以取得 /upload 路徑
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
-=======
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
->>>>>>> user2
 
 app.use(router)
 
