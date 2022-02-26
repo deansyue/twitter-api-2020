10072f810b65cbf2235301f3444590759341d6a8
deansyue <c1318131801@gmail.com>
2022-02-26 16:25:00 +0800

    feat: 新增user router 功能

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,23 +1,17 @@
 const express = require('express')
-const passport = require('../../config/passport')
 const userController = require('../../controllers/userController')
 const router = express.Router()
-const { authenticated } = require('../../middleware/auth')
 const upload = require('../../middleware/multer')
 
-//登入註冊功能
-router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-router.post('/', userController.signUp)
-
 // user
-router.get('/users/:id/replied_tweets', userController.getRepliedTweets)
-router.get('/users/:id/tweets', authenticated, userController.getUserTweets)
-router.get('/users/:id/likes', authenticated, userController.getLikedTweet)
-router.get('/users/:id/followings', authenticated, userController.getFollowings)
-router.get('/users/:id/followers', authenticated, userController.getFollowers)
-router.put('/users/:id', authenticated, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1}]), userController.putUser)
-router.put('/users/:id/edit', authenticated, userController.editUser)
-router.get('/users/:id', authenticated, userController.getUser)
+router.get('/:id/replied_tweets', userController.getRepliedTweets)
+router.get('/:id/tweets', userController.getUserTweets)
+router.get('/:id/likes', userController.getLikedTweet)
+router.get('/:id/followings', userController.getFollowings)
+router.get('/:id/followers', userController.getFollowers)
+router.put('/:id', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUser)
+router.put('/:id/edit', userController.editUser)
+router.get('/:id', userController.getUser)
 router.get('/', (req, res) => res.send('Hello World!'))
 
 module.exports = router
