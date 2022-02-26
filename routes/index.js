40bd3a6dbc41869e03792f893ac194520534814b
deansyue <c1318131801@gmail.com>
2022-02-26 16:22:21 +0800

    modify 刪除admin路由的登入者權限認

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,17 @@ const { errorHandler } = require('../middleware/error-handler')
 const user = require('./modules/user')
 const admin = require('./modules/admin')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const userController = require('../controllers/userController')
+const adminController = require('../controllers/adminController')
 
+//登入註冊功能路由
+router.post('/users/signin', userController.signIn)
+router.post('/users', userController.signUp)
+router.post('/admin/signin', adminController.signIn)
+
+// 功能路由
 router.use('/users', authenticated, user)
-router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/admin', authenticated, admin)
 router.use('/', errorHandler)
 
 module.exports = router
