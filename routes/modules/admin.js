a19eb08f8611f59c0dac5219cbf0017e4b4f5370
deansyue <c1318131801@gmail.com>
2022-02-26 16:23:51 +0800

    feat: 新增admin router 功能

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const passport = require('../../config/passport')
 const adminController = require('../../controllers/adminController')
 const router = express.Router()
-const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
 
 // admin 
-router.post('/signin', passport.authenticate('local', { session: false }), adminController.signIn)
-router.get('/users', authenticated, authenticatedAdmin, adminController.getUsers)
-router.get('/tweets', authenticated, authenticatedAdmin, adminController.getTweets)
-router.delete('/tweets/:id', authenticated, authenticatedAdmin, adminController.deleteTweet)
+router.get('/users', adminController.getUsers)
+router.get('/tweets', adminController.getTweets)
+router.delete('/tweets/:id', adminController.deleteTweet)
 
 
 module.exports = router
